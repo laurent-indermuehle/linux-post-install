fe1fc0834616e5e97324e7773bf954aa60b45500
Laurent Indermühle <laurent.indermuehle@pm.me>
2018-11-30 21:54:12 +0100

    Early stage, not tested!!!

---
+++ README.md
@@ -0,0 +1,12 @@
+Forked from https://gist.github.com/danidiaz/4486be84912ab71a9802
+
+## Prerequisites:
+
+1) Copy your ssh pubkey in /root.ssh/authorized_keys
+2) Configure contants bellow
+
+
+After this script you should:
+
+1) Change root password
+2) Change $USERNAME password
---
+++ centos7-postinstall.sh
@@ -0,0 +1,168 @@
+#!/bin/bash
+
+source config.sh
+# Todo controle si le fichier existe
+
+readonly HOSTNAMEFULL="$COMPUTERNAME"."$HOSTNAME"
+ 
+# http://linuxcommand.org/wss0150.php
+function error_exit
+{
+    echo "$1" 1>&2
+    exit 1
+} 
+ 
+# http://unix.stackexchange.com/questions/70859/why-doesnt-sudo-su-in-a-shell-script-run-the-rest-of-the-script-as-root 
+if [ "$(whoami)" = root ]; then
+
+    # for OVH only:
+    cp /root/.ssh/authorized_keys2 /root/.ssh/authorized_keys
+    # or using AUTHORIZEDKEYS
+    #echo "$AUTHORIZEDKEYS" > /root/.ssh/authorized_keys
+
+    chmod 600 /root/.ssh/authorized_key
+    sed -i 's/RSAAuthentication no/RSAAuthentication yes/g' /etc/ssh/sshd_config
+    sed -i 's/PubkeyAuthentication no/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
+    sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+    sed -i 's/GSSAPIAuthentication yes/GSSAPIAuthentication no/g' /etc/ssh/sshd_config
+    # sed -i 's/UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config
+    systemctl restart sshd
+ 
+    # Firewalld
+    yum -y install firewalld
+    systemctl enable firewalld
+    systemctl start firewalld
+    # 2018-09-29 Laurent Indermühle: Why do you want to add ssh to drop zone ? without source or interface, I don't see the point
+    # firewall-cmd --permanent --zone=drop --add-service=ssh
+    # firewall-cmd --set-default-zone=drop
+    # Allow OVH to monitor the server
+    firewall-cmd --zone=public --permanent --add-interface=eth0  # this will activate zone public for main network link and thus allow ICMP (ping)
+    firewall-cmd --zone=public --permanent --add-port=6100-6200/udp
+    firewall-cmd --reload
+
+    # Hostname
+    hostnamectl set-hostname "$COMPUTERNAME" --pretty
+    hostnamectl set-hostname "$HOSTNAME" --static
+
+    # Yum-cron
+    yum -y install yum-cron sendmail
+    # https://askubuntu.com/questions/76808/how-do-i-use-variables-in-a-sed-command
+    sed -i "s/system_name = None/system_name = $HOSTNAMEFULL/g" /etc/yum/yum-cron.conf
+    sed -i 's/emit_via = stdio/emit_via = email/g' /etc/yum/yum-cron.conf
+    sed -i "s/email_from = root@localhost/email_from = root@$HOSTNAMEFULL/g" /etc/yum/yum-cron.conf
+    sed -i "s/email_to = root/email_to = $USEREMAIL/g" /etc/yum/yum-cron.conf
+    # Remember, on CentOs you can only use updade_cmd = default
+    # apply_updates = no it means you have to login an apply updates
+    systemctl start yum-cron.service
+    systemctl enable yum-cron.service
+    systemctl start sendmail.service
+    systemctl enable sendmail.service
+
+    # Install base packages
+    yum -y install epel-release make gcc git git-daemon nmap p7zip
+
+    # Install personnal preferences packages
+    yum -y install vim-enhanced tmux links mlocate
+    
+    # Docker
+    yum -y docker docker-registry
+    # make the Docker registry listen only on localhost
+    #sed -i 's/REGISTRY_ADDRESS=0\.0\.0\.0/REGISTRY_ADDRESS=127.0.0.1/g' /etc/sysconfig/docker-registry
+
+    # Httpd (Apache)
+    # todo
+
+    # MariaDB
+    # todo
+
+    # Webmin? test first
+
+    ## Installing Go
+    #cd /usr/local
+    #wget https://storage.googleapis.com/golang/go1.4.linux-amd64.tar.gz
+    #tar -zxvf go1.4.linux-amd64.tar.gz
+    #cd /root
+    
+    # Creating and configuring $USERNAME user
+    useradd -m "$USERNAME"
+    gpasswd -a "$USERNAME" wheel
+    mkdir /home/"$USERNAME"/.ssh
+    cp /root/.ssh/authorized_keys /home/"$USERNAME"/.ssh/authorized_keys
+    chown -R "$USERNAME":"$USERNAME" /home/"$USERNAME"/.ssh
+    chmod 500 /home/"$USERNAME"/.ssh
+    chmod 400 /home/"$USERNAME"/.ssh/authorized_keys
+
+    # Add Docker right for USERNAME
+    # https://www.projectatomic.io/blog/2015/08/why-we-dont-let-non-root-users-run-docker-in-centos-fedora-or-rhel/
+    # TODO
+    # something like echo username(add constant here) ALL=(ALL) /usr/bin/docker >> /etc/sudoers
+ 
+    cp "$0" /home/"$USERNAME"/centos7-postinstall.sh
+    chown "$USERNAME":"$USERNAME" /home/"$USERNAME"/centos7-postinstall.sh
+    chmod u+x /home/"$USERNAME"/centos7-postinstall.sh
+    su - -c /home/"$USERNAME"/centos7-postinstall.sh "$USERNAME"
+    rm -rf /home/"$USERNAME"/centos7-postinstall.sh
+    
+    # Haskdev can shut the machine down
+    # http://www.garron.me/en/linux/visudo-command-sudoers-file-sudo-default-editor.html
+    #echo "$USERNAME ALL= NOPASSWD: /sbin/shutdown -h now, /usr/bin/lastb" >> /etc/sudoers
+ 
+    # Starting Docker
+    # service docker start
+    # service docker-registry start
+ 
+elif [ "$(whoami)" = "$USERNAME" ]; then
+ 
+    # Configuring git
+    git config --global user.name "$USERNAME"
+    git config --global user.email "$USEREMAIL"
+    git config --global push.default simple
+ 
+    # Configuring vim
+    #curl -L -O https://raw.githubusercontent.com/danidiaz/miscellany/master/linux/.vimrc
+     
+    #mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+    #curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+     
+    #cd .vim/bundle
+    #git clone https://github.com/Shougo/unite.vim.git
+    #git clone https://github.com/tpope/vim-repeat
+    #git clone https://github.com/tpope/vim-surround.git
+    #git clone https://github.com/tommcdo/vim-exchange.git
+    #git clone https://github.com/justinmk/vim-sneak.git
+    #git clone https://github.com/sirver/ultisnips
+    #git clone https://github.com/dag/vim2hs
+    #git clone https://github.com/fatih/vim-go
+    #git clone https://github.com/michaeljsmith/vim-indent-object
+    #cd "$HOME"
+    #
+    #mkdir .vim/colors
+    #cd .vim/colors
+    #curl -L -O https://raw.githubusercontent.com/fugalh/desert.vim/master/desert.vim
+    #cd "$HOME"
+ 
+    # Configuring tmux
+    # Note that prefix is set to C-j
+    curl -L -O "$TMUXCONFURL"
+    mv tmux.conf .tmux.conf
+     
+    # Necessary for tmux to work
+    # echo export LD_LIBRARY_PATH=/usr/local/lib >> "$HOME"/.bash_profile
+ 
+    # Settign go path
+    #echo "PATH=\$PATH:/usr/local/go/bin" >> .bash_profile
+    #
+    #mkdir go
+    #mkdir go/src
+    #mkdir go/pkg
+    #mkdir go/bin
+    #
+    #echo "GOPATH=\$PATH:\$HOME/go" >> .bash_profile
+    #echo "export GOPATH" >> .bash_profile
+    #echo "PATH=\$PATH:\$HOME/go/bin" >> .bash_profile
+ 
+else
+ 
+    echo "Should not be here!!!"
+ 
+fi
